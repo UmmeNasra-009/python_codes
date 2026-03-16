8625202610bcd0d4f6c4af954391de79d2859dc9
UmmeNasra-009 <ummenasrashaikh@gmail.com>
2026-03-16 22:05:47 +0530

    Create tuple2.py

---
+++ tuple2.py
@@ -0,0 +1,9 @@
+students = (
+    ("Ali", 20, "BCA"),
+    ("Sara", 21, "BBA"),
+    ("John", 22, "BTech")
+)
+
+print("First student:", students[0])
+print("Second student name:", students[1][0])
+print("Third student course:", students[2][2])
