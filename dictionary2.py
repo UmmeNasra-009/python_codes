5002dfbeff72da92dfcb6c40960f72d092bca4b3
UmmeNasra-009 <ummenasrashaikh@gmail.com>
2026-03-16 21:59:02 +0530

    Create dictionary2.py

---
+++ dictionary2.py
@@ -0,0 +1,20 @@
+students = {
+    "student1": {
+        "name": "Ali",
+        "age": 20,
+        "roll_no": 101
+    },
+    "student2": {
+        "name": "Sara",
+        "age": 21,
+        "roll_no": 102
+    },
+  "student3":{
+        "name":"Alina",
+         "age":22,
+        "roll_no":103
+}
+}
+
+print(students["student1"]["name"])
+print(students["student2"]["age"])
